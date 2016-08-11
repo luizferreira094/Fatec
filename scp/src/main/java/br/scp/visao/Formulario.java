fbbb5aaf9c930b596d8bdadbf9184cf449a1c3f6
edson <engsoftware1@gmail.com>
2016-08-11 19:13:37 -0300

    Ajuste na interface de usuario

--- scp/src/main/java/br/scp/visao/Formulario.java
+++ scp/src/main/java/br/scp/visao/Formulario.java
@@ -34,6 +34,11 @@ public class Formulario extends JFrame {
 	private JTextField txtEndereco= null;
 	private JTextField txtTelefone= null;
 	private JTextField txtHI= null;
+	private JTextField txtHT= null;
+	private JTextField txtConjunto1= null;
+	private JTextField txtConjunto2= null;
+	private JTextField txtConjunto3= null;
+	private JTextField txtTemperaturaMaxima= null;
 	private JTextField txtMensagem = null;
 	Controle controle;
 	public Formulario() {
@@ -94,6 +99,12 @@ public class Formulario extends JFrame {
 			pnlCentral.add(getTxtEndereco(), null);
 			pnlCentral.add(getTxtTelefone(), null);
 			pnlCentral.add(getTxtHI(), null);
+			pnlCentral.add(getTxtHT(), null);
+			pnlCentral.add(getTxtConjunto1(), null);
+			pnlCentral.add(getTxtConjunto2(), null);
+			pnlCentral.add(getTxtConjunto3(), null);
+			pnlCentral.add(getTxtTemperaturaMaxima(), null);
+			
 			pnlCentral.add(getTxtMensagem(), null);
 			
 
@@ -104,7 +115,7 @@ public class Formulario extends JFrame {
 		if (txtCPF == null) {
 			txtCPF = new JTextField();
 			txtCPF.setSize(95, 40); // largura x altura
-			txtCPF.setLocation(150, 50); // horizontal x vertical
+			txtCPF.setLocation(150, 10); // horizontal x vertical
 			txtCPF.setBorder(javax.swing.BorderFactory.createTitledBorder(
 					null, "CNPJ",
 					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
@@ -119,7 +130,7 @@ public class Formulario extends JFrame {
 		if (txtNome == null) {
 			txtNome = new JTextField();
 			txtNome.setSize(285, 40); // largura x altura
-			txtNome.setLocation(150, 100); // horizontal x vertical
+			txtNome.setLocation(150, 60); // horizontal x vertical
 			txtNome.setBorder(javax.swing.BorderFactory.createTitledBorder(
 					null, "Razao Social",
 					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
@@ -134,7 +145,7 @@ public class Formulario extends JFrame {
 		if (txtEndereco == null) {
 			txtEndereco = new JTextField();
 			txtEndereco.setSize(285, 40); // largura x altura
-			txtEndereco.setLocation(150, 150); // horizontal x vertical
+			txtEndereco.setLocation(150, 110); // horizontal x vertical
 			txtEndereco.setBorder(javax.swing.BorderFactory.createTitledBorder(
 					null, "Endereco",
 					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
@@ -149,7 +160,7 @@ public class Formulario extends JFrame {
 		if (txtTelefone == null) {
 			txtTelefone = new JTextField();
 			txtTelefone.setSize(95, 40); // largura x altura
-			txtTelefone.setLocation(150, 200); // horizontal x vertical
+			txtTelefone.setLocation(150, 160); // horizontal x vertical
 			txtTelefone.setBorder(javax.swing.BorderFactory.createTitledBorder(
 					null, "Telefone",
 					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
@@ -164,7 +175,7 @@ public class Formulario extends JFrame {
 		if (txtHI == null) {
 			txtHI = new JTextField();
 			txtHI.setSize(125, 40); // largura x altura
-			txtHI.setLocation(150, 250); // horizontal x vertical
+			txtHI.setLocation(150, 210); // horizontal x vertical
 			txtHI.setBorder(javax.swing.BorderFactory.createTitledBorder(
 					null, "Horario de inicio",
 					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
@@ -175,11 +186,86 @@ public class Formulario extends JFrame {
 		}
 		return txtHI;
 	}
+	public JTextField getTxtHT() {
+		if (txtHT == null) {
+			txtHT = new JTextField();
+			txtHT.setSize(125, 40); // largura x altura
+			txtHT.setLocation(290, 210); // horizontal x vertical
+			txtHT.setBorder(javax.swing.BorderFactory.createTitledBorder(
+					null, "Horario de termino",
+					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+					javax.swing.border.TitledBorder.DEFAULT_POSITION,
+					new java.awt.Font("Dialog", java.awt.Font.BOLD, 11), null));
+			txtHT.setFont(new java.awt.Font("Dialog",
+					java.awt.Font.PLAIN, 12));
+		}
+		return txtHT;
+	}
+	public JTextField getTxtConjunto1() {
+		if (txtConjunto1 == null) {
+			txtConjunto1 = new JTextField();
+			txtConjunto1.setSize(75, 40); // largura x altura
+			txtConjunto1.setLocation(150, 260); // horizontal x vertical
+			txtConjunto1.setBorder(javax.swing.BorderFactory.createTitledBorder(
+					null, "Conjunto1",
+					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+					javax.swing.border.TitledBorder.DEFAULT_POSITION,
+					new java.awt.Font("Dialog", java.awt.Font.BOLD, 11), null));
+			txtConjunto1.setFont(new java.awt.Font("Dialog",
+					java.awt.Font.PLAIN, 12));
+		}
+		return txtConjunto1;
+	}
+	public JTextField getTxtConjunto2() {
+		if (txtConjunto2 == null) {
+			txtConjunto2 = new JTextField();
+			txtConjunto2.setSize(75, 40); // largura x altura
+			txtConjunto2.setLocation(250, 260); // horizontal x vertical
+			txtConjunto2.setBorder(javax.swing.BorderFactory.createTitledBorder(
+					null, "Conjunto 2",
+					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+					javax.swing.border.TitledBorder.DEFAULT_POSITION,
+					new java.awt.Font("Dialog", java.awt.Font.BOLD, 11), null));
+			txtConjunto2.setFont(new java.awt.Font("Dialog",
+					java.awt.Font.PLAIN, 12));
+		}
+		return txtConjunto2;
+	}
+	public JTextField getTxtConjunto3() {
+		if (txtConjunto3 == null) {
+			txtConjunto3 = new JTextField();
+			txtConjunto3.setSize(75, 40); // largura x altura
+			txtConjunto3.setLocation(350, 260); // horizontal x vertical
+			txtConjunto3.setBorder(javax.swing.BorderFactory.createTitledBorder(
+					null, "Conjunto 3",
+					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+					javax.swing.border.TitledBorder.DEFAULT_POSITION,
+					new java.awt.Font("Dialog", java.awt.Font.BOLD, 11), null));
+			txtConjunto3.setFont(new java.awt.Font("Dialog",
+					java.awt.Font.PLAIN, 12));
+		}
+		return txtConjunto3;
+	}
+	public JTextField getTxtTemperaturaMaxima() {
+		if (txtTemperaturaMaxima == null) {
+			txtTemperaturaMaxima = new JTextField();
+			txtTemperaturaMaxima.setSize(140, 40); // largura x altura
+			txtTemperaturaMaxima.setLocation(150, 300); // horizontal x vertical
+			txtTemperaturaMaxima.setBorder(javax.swing.BorderFactory.createTitledBorder(
+					null, "Temperatura Maxima",
+					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+					javax.swing.border.TitledBorder.DEFAULT_POSITION,
+					new java.awt.Font("Dialog", java.awt.Font.BOLD, 11), null));
+			txtTemperaturaMaxima.setFont(new java.awt.Font("Dialog",
+					java.awt.Font.PLAIN, 12));
+		}
+		return txtTemperaturaMaxima;
+	}
 	public JTextField getTxtMensagem() {
 		if (txtMensagem == null) {
 			txtMensagem = new JTextField();
 			txtMensagem.setSize(550, 40); // largura x altura
-			txtMensagem.setLocation(10, 315); // horizontal x vertical
+			txtMensagem.setLocation(10, 340); // horizontal x vertical
 			txtMensagem.setBorder(javax.swing.BorderFactory.createTitledBorder(
 					null, "Mensagem",
 					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
